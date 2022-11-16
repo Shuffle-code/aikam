245995226969163badbf49582a9ff3074e452745
79130 <malugn-evg-nsk@yandex.ru>
2022-11-16 13:15:53 +0700

    init app

---
+++ src/main/java/test/example/aikam/AikamApplication.java
@@ -0,0 +1,13 @@
+package test.example.aikam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AikamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AikamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/example/aikam/AikamApplicationTests.java
@@ -0,0 +1,13 @@
+package test.example.aikam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AikamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
